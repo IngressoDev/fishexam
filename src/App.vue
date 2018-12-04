bd46159ea4f99387060fa8afb4c236c44166e9a8
Spanier <43876434+IngressoDev@users.noreply.github.com>
2018-12-04 14:50:34 +0100

    Swich to VueJS

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Fish</span>
+        <span class="font-weight-light">Exam</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!-- <v-menu offset-y>
+        <v-btn slot="activator">Lernen</v-btn>
+        <v-list>
+          <v-list-tile to="/training/questions">
+            <v-list-tile-title>Fragen</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu> -->
+      <v-menu offset-y>
+        <v-btn slot="activator">Prüfung</v-btn>
+        <v-list>
+          <v-list-tile to="/exam/questions">
+            <v-list-tile-title>Fragen</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      //
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+.v-input--selection-controls .v-input__control {
+  width: 100% !important;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.v-radio {
+  height: 100% !important;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.v-input--selection-controls {
+  margin-top: 0 !important;
+  padding-top: 0 !important;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -20,6 +20,16 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/training/questions',
+      name: 'questionstraining',
+      component: () => import('./views/Questions.vue')
+    },
+    {
+      path: '/exam/questions',
+      name: 'questionsexam',
+      component: () => import('./views/Questions.vue')
     }
   ]
 })
